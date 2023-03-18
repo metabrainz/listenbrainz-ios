6209be03ab29048508efd6b915f783f7fd4be769
Akshat Tiwari <51470769+akshaaatt@users.noreply.github.com>
2023-03-19 01:23:40 +0530

    Initial Commit

---
+++ Listenbrainz/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Listenbrainz
+//
+//  Created by avataar on 19/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Listenbrainz/ListenbrainzApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListenbrainzApp.swift
+//  Listenbrainz
+//
+//  Created by avataar on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ListenbrainzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

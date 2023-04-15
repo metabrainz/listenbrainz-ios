1371f240449f5f9eada319cfd1f67b29f75cc0bb
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2023-04-16 02:36:59 +0530

    created bottom  toolbar

---
+++ Listenbrainz/UI/Screens/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  Listenbrainz
+//
+//  Created by Gaurav Bhardwaj on 16/04/23.
+//
+
+import SwiftUI
+import UIKit
+
+struct ContentView: View {
+    
+    init() {
+        UITabBar.appearance().unselectedItemTintColor = UIColor.systemBlue
+        UITabBar.appearance().layer.borderColor = UIColor.clear.cgColor
+        UITabBar.appearance().clipsToBounds = true
+    }
+    
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            TabView {
+                HomeView()
+                    .tabItem {
+                        Label("Home", systemImage: "house.fill")
+                    }
+                VStack(spacing: 0) {
+                    PlayerView()
+                        .edgesIgnoringSafeArea(.all)
+                    
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 6, alignment: .center)
+                }
+                    .tabItem {
+                        
+                        
+                        Label("Player", systemImage: "opticaldisc.fill")
+                    }
+                ListensView()
+                    .tabItem {
+                        Label("Listen", systemImage: "person.wave.2")
+                    }
+                VStack(spacing: 0) {
+                                  ProfileView()
+                                      .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 6, alignment: .center)
+                              }
+                              .tabItem {
+                                  
+                                  Label("Profile", systemImage: "beats.headphones")
+                              }
+            
+            }
+            .accentColor(Color.orange)
+        }
+
+    }
+}
+
---
+++ Listenbrainz/UI/Screens/HomeView.swift
@@ -0,0 +1,17 @@
+//
+//  HomeView.swift
+//  Listenbrainz
+//
+//  Created by Gaurav Bhardwaj on 16/04/23.
+//
+
+import SwiftUI
+
+struct HomeView : View{
+    
+    var body: some View{
+        
+        Text("Home")
+    }
+    
+}

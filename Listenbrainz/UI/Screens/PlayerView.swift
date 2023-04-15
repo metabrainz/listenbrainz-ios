ddc8c247205b7a7b437d73b060cf53416d1b62a5
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2023-04-16 02:36:59 +0530

    created bottom  toolbar

---
+++ Listenbrainz/UI/Screens/PlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerView.swift
+//  Listenbrainz
+//
+//  Created by Gaurav Bhardwaj on 16/04/23.
+//
+
+
+import SwiftUI
+
+struct PlayerView : View{
+    
+    var body: some View{
+        
+        Text("Player")
+    }
+    
+}

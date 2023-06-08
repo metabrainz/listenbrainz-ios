49673dc883f05db68769de1b87ca7291e62a2a5f
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2023-06-08 21:20:30 +0530

    Made Home Section

--- Listenbrainz/UI/Screens/HomeView.swift
+++ Listenbrainz/UI/Screens/HomeView.swift
@@ -7,11 +7,107 @@
 
 import SwiftUI
 
-struct HomeView : View{
-    
-    var body: some View{
-        
-        Text("Home")
+
+struct AdaptiveImage: View {
+    @Environment(\.colorScheme) var colorScheme
+    let light: Image
+    let dark: Image
+
+    @ViewBuilder var body: some View {
+        if colorScheme == .light {
+            light
+        } else {
+            dark
+        }
     }
-    
+}
+struct HomeView : View{
+
+  var body: some View{
+
+    NavigationView{
+      ScrollView{
+        VStack(alignment: .center ,spacing: 0){
+
+
+
+          AdaptiveImage(light: Image("listenBrainzLight"), dark: Image("listenBrainzDark"))
+
+
+
+          HStack(spacing: 0){
+            Text("Listen")
+              .foregroundColor(.init(red: 0.20, green: 0.19, blue: 0.42))
+
+            Text("Brainz")
+              .foregroundColor(.init(red: 0.86, green: 0.49, blue: 0.29))
+
+          }
+          .font(.largeTitle)
+          .fontWeight(.bold)
+
+        }
+        .padding(.all)
+        .navigationTitle("ListenBrainz")
+
+        VStack(spacing: 10){
+          HStack(alignment: .center, spacing: 0) {
+            Image(systemName: "radio")
+              .resizable()
+              .scaledToFit()
+              .frame(width: 80, height: 65).cornerRadius(16)
+              .foregroundColor(.init(red: 0.86, green: 0.49, blue: 0.29))
+            VStack(alignment: .leading, spacing: 8) {
+              Text("Year in Music")
+                .lineLimit(1)
+                .font(.headline)
+
+              Text("Your Whole Year Summarized")
+
+
+            }
+            .padding(.leading, 12)
+            Spacer()
+          }
+          .padding(12)
+          .background(Color.gray.opacity(0.15))
+
+
+          HStack(alignment: .center, spacing: 0) {
+            Image(systemName: "mic")
+              .resizable()
+              .scaledToFit()
+              .frame(width: 80, height: 65).cornerRadius(16)
+              .foregroundColor(.init(red: 0.86, green: 0.49, blue: 0.29))
+            VStack(alignment: .leading, spacing: 8) {
+              Text("News")
+                .lineLimit(1)
+                .font(.headline)
+
+              Text("ListenBrainz News Updates")
+
+
+            }
+            .padding(.leading, 12)
+            Spacer()
+          }
+          .padding(12)
+          .background(Color.gray.opacity(0.15))
+
+        }
+
+      }
+
+
+      }
+    }
+  }
+
+
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+
+    }
 }

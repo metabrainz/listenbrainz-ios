a10f5b535544f3a2b9e1abb774906f16c3e0e0c0
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2023-08-18 14:49:52 +0530

    Added DocumentPicker
    Added DocumentPicker for importing audio file and matching that audio file with the help of updated ShazamViewModel.

--- Listenbrainz/UI/Screens/PlayerView.swift
+++ Listenbrainz/UI/Screens/PlayerView.swift
@@ -17,8 +17,10 @@ extension SHMatchedMediaItem: Identifiable {
 }
 
 struct PlayerView: View {
-
-    @StateObject private var viewModel: ShazamViewModel = .init()
+  @State private var fileName = ""
+  @State private var openFile = false
+  @StateObject private var viewModel: ShazamViewModel = .init()
+  @State private var animateShazamButton = false
 
     var body: some View {
         content
@@ -39,28 +41,92 @@ struct PlayerView: View {
     }
     else {
       VStack(spacing:40) {
-        Image(systemName: "shazam.logo.fill")
-          .resizable()
-          .frame(width: 150, height: 150)
-          .foregroundColor(Color.color_2)
-        Button(action: shazam) {
-          Text("Shazam")
-            .padding()
+
+        Text("Tap icon to Shazam")
+          .font(.largeTitle)
+
+        Button(action: {
+                       animateShazamButton.toggle()
+                       shazam()
+                   }) {
+                       Image(systemName: "shazam.logo.fill")
+                           .resizable()
+                           .frame(width: 150, height: 150)
+                           .foregroundColor(Color.blue.opacity(0.8))
+                           .scaleEffect(animateShazamButton ? 0.9 : 1.0)
+                   }
+                   .onAppear {
+                       startAnimationTimer()
+                   }
+
+
+        Button(action: {
+          openFile.toggle()
+
+        }, label: {
+            Text("Import song")
+                .foregroundColor(.white)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 35)
+                .background(Color.color_2.opacity(0.7))
+                .clipShape(Capsule())
+        })
+        Text(fileName)
+          .foregroundColor(Color.color_1)
+        Button {
+          if viewModel.selectedAudioURL != nil {
+                 viewModel.startMatching()
+             }
+        } label: {
+          Text("Match")
             .foregroundColor(.white)
-            .background(Color.color_1)
-            .cornerRadius(10)
+            .padding(.vertical, 10)
+            .padding(.horizontal, 35)
+            .background(Color.color_1.opacity(0.7))
+            .clipShape(Capsule())
         }
-        Button(viewModel.isMatching ? "Matching…" : "Match Custom audio", action: viewModel.startMatching)
-                 .disabled(viewModel.isMatching)
-                 .tint(.blue)
-                 .padding(.vertical)
-                 .buttonStyle(.borderless)
-                 .offset(y:90)
+
       }
+      .fileImporter(isPresented: self.$openFile, allowedContentTypes: [.audio]) { result in
+          do {
+              let fileURL = try result.get()
+              self.viewModel.selectedAudioURL = fileURL
+              self.fileName = fileURL.lastPathComponent
+              print("Selected audio URL: \(fileURL)")
+          } catch {
+              print(error.localizedDescription)
+          }
+      }
+
     }
   }
+  private func startAnimationTimer() {
+    Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+             withAnimation(.easeInOut(duration: 0.4)) {
+                 animateShazamButton.toggle()
+             }
+         }
+     }
     func shazam() {
         viewModel.start()
+    }
+}
+
+struct DocumentManager: FileDocument {
+    var url: String
+    static var readableContentTypes: [UTType] { [.audio] }
+
+    init(url: String)  {
+        self.url = url
+    }
+
+    init(configuration: ReadConfiguration) throws {
+        url = ""
+    }
+
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        let file = try! FileWrapper(url: URL(fileURLWithPath: url), options: .immediate)
+        return file
     }
 }
 

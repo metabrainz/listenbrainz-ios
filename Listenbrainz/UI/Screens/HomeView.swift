dc41b0cd32519a76ecd2b7f032e2387e930d3b94
Akshat Tiwari <51470769+akshaaatt@users.noreply.github.com>
2023-11-18 17:50:08 +0530

    File updates

--- Listenbrainz/UI/Screens/HomeView.swift
+++ Listenbrainz/UI/Screens/HomeView.swift
@@ -144,15 +144,6 @@ struct HomeView : View{
 }
 
 
-
-
-extension Color{
-
-  static let color_1 = Color(red: 0.86, green: 0.49, blue: 0.29)
-  static let color_2 = Color(red: 0.20, green: 0.19, blue: 0.42)
-
-}
-
 struct HomeView_Previews: PreviewProvider {
   static var previews: some View {
     HomeView()
---
+++ Listenbrainz/Utils/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  Listenbrainz
+//
+//  Created by avataar on 18/11/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+
+  static let color_1 = Color(red: 0.86, green: 0.49, blue: 0.29)
+  static let color_2 = Color(red: 0.20, green: 0.19, blue: 0.42)
+
+}

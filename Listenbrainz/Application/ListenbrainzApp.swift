a2d8ce07e9551b2a9cadff7d9ec2ccae4a102ce5
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2023-12-07 04:51:59 +0530

    Implement onBoarding at entrypoint

--- Listenbrainz/Application/ListenbrainzApp.swift
+++ Listenbrainz/Application/ListenbrainzApp.swift
@@ -12,32 +12,40 @@ import SpotifyiOS
 struct ListenbrainzApp: App {
     @StateObject var homeViewModel =  HomeViewModel(repository: HomeRepositoryImpl())
     @StateObject var feedViewModel = FeedViewModel(repository: FeedRepositoryImpl())
+    @AppStorage("isOnboarding") var isOnboarding: Bool = true
+
 
     //@StateObject var spotifyManager = SpotifyManager()
     @AppStorage("isDarkMode") private var isDarkMode = false
     @AppStorage("userName") private var userName:String = ""
     @AppStorage("userToken") private var userToken:String = ""
     
-    var body: some Scene {
-        WindowGroup {
-            NavigationView {
-                ContentView()
-                    .environmentObject(homeViewModel)
-                    .environmentObject(feedViewModel)
-                    .preferredColorScheme(isDarkMode ? .dark : .light)
-                    //.environmentObject(spotifyManager)
-                    .onAppear {
-                        //handleSpotifySession()
-                        homeViewModel.requestMusicData(userName: userName)
-                        feedViewModel.fetchFeedEvents(username: userName, userToken: userToken)
-                    }
-                    .onOpenURL { url in
-                        //spotifyManager.sessionManager.application(UIApplication.shared, open: url, options: [:])
-                    }
+  var body: some Scene {
+    WindowGroup {
+      
+      NavigationView {
+        if isOnboarding {
+          OnBoardingView()
+        }
+        else{
+          ContentView()
+            .environmentObject(homeViewModel)
+            .environmentObject(feedViewModel)
+            .preferredColorScheme(isDarkMode ? .dark : .light)
+          //.environmentObject(spotifyManager)
+            .onAppear {
+              //handleSpotifySession()
+              homeViewModel.requestMusicData(userName: userName)
+              feedViewModel.fetchFeedEvents(username: userName, userToken: userToken)
+            }
+            .onOpenURL { url in
+              //spotifyManager.sessionManager.application(UIApplication.shared, open: url, options: [:])
             }
         }
+      }
     }
-    
+  }
+
     private func handleSpotifySession() {
         //spotifyManager.connect()
     }

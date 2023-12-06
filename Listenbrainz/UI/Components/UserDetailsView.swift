6da21b0db0bc63be3629a08cd4b58b44b50f84f7
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2023-12-07 04:53:04 +0530

    View for token and userName

---
+++ Listenbrainz/UI/Components/UserDetailsView.swift
@@ -0,0 +1,85 @@
+//
+//  UserDetailsView.swift
+//  Listenbrainz
+//
+//  Created by Gaurav Bhardwaj on 04/12/23.
+//
+
+
+import SwiftUI
+
+struct UserDetailsView: View {
+
+  // MARK: - PROPERTIES
+  @AppStorage("isOnboarding") var isOnboarding: Bool?
+  @AppStorage("userToken") private var userToken: String = ""
+  @AppStorage("userName") private var userName: String = ""
+
+  var body: some View {
+    VStack {
+      Spacer()
+
+      HStack {
+        Text("User Name")
+          .font(.title)
+          .fontWeight(.bold)
+          .padding(.leading)
+        Spacer()
+      }
+
+      VStack {
+        TextField("User Name", text: $userName)
+          .font(.system(size: 18))
+
+      }
+      .padding()
+      .background(RoundedRectangle(cornerRadius: 15).stroke(Color.gray.opacity(0.7)))
+
+      HStack {
+        Text("User Token")
+          .font(.title)
+          .fontWeight(.bold)
+          .padding(.top)
+          .padding(.leading)
+        Spacer()
+      }
+
+      VStack {
+        TextField("User Token", text: $userToken)
+          .font(.system(size: 18))
+      }
+      .padding()
+      .background(RoundedRectangle(cornerRadius: 15).stroke(Color.gray.opacity(0.7)))
+
+      Text("Enter User Token from https://listenbrainz.org/profile/")
+        .font(.callout)
+        .foregroundColor(.gray)
+        .multilineTextAlignment(.center)
+        .padding(.horizontal)
+        .padding(.bottom, 20)
+
+      Spacer()
+
+      Button {
+        isOnboarding = false
+      } label: {
+        Text("Get Started")
+          .font(.headline)
+          .padding()
+          .frame(maxWidth: .infinity)
+          .background(Color.color_2.opacity(0.7))
+          .foregroundColor(.white)
+          .cornerRadius(20)
+      }
+
+      Spacer()
+    }
+    .padding()
+  }
+}
+
+struct UserDetailsView_Previews: PreviewProvider {
+  static var previews: some View {
+    UserDetailsView()
+  }
+}

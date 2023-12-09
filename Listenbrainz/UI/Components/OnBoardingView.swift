8b5ea9926aafd01d3e6a60983f9e6fb023d1fdbc
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2023-12-09 07:45:14 +0530

    Update onBoarding

--- Listenbrainz/UI/Components/OnBoardingView.swift
+++ Listenbrainz/UI/Components/OnBoardingView.swift
@@ -1,53 +1,64 @@
 import SwiftUI
 
-struct OnBoardingView : View{
-  @State private var iconsScale: CGFloat = 0.5 
+struct OnBoardingView: View {
+  @State private var iconsScale: CGFloat = 0.5
 
-
-  var body: some View{
+  var body: some View {
     VStack {
-
       Spacer()
+
       Text("Welcome To ListenBrainz")
         .font(.system(size: 48, weight: .semibold))
         .padding(.bottom)
+
       Spacer()
 
-      HStack {
-        LBIcon2()
-          .fill(Color.color_2)
+      HStack(spacing: -120) {
+        Image("secondaryIcon")
+          .resizable()
+          .scaledToFit()
           .scaleEffect(iconsScale)
-          .onAppear {
+          .frame(width: 250 , height: 250 )
+          .onTapGesture {
             withAnimation(.easeInOut(duration: 1.0)) {
               iconsScale = 1.0
             }
           }
 
-        LBIcon1()
-          .fill(Color.color_1)
+        Image("primaryIcon")
+          .resizable()
+          .scaledToFit()
           .scaleEffect(iconsScale)
-          .onAppear {
+          .frame(width: 250, height:250)
+          .onTapGesture {
             withAnimation(.easeInOut(duration: 1.0)) {
               iconsScale = 1.0
             }
           }
       }
-      .frame(width: 300, height: 300)
+
       Spacer()
 
       NavigationLink(destination: UserDetailsView()) {
         Text("Next")
           .font(.headline)
       }
+      .navigationBarTitle("", displayMode: .inline)
+      .navigationBarHidden(true)
+
+
       Spacer()
     }
+    .onAppear {
+      withAnimation(.easeInOut(duration: 1.0)) {
+        iconsScale = 1.0
+      }
+    }
   }
-
 }
 
 struct OnBoardingView_Previews: PreviewProvider {
   static var previews: some View {
     OnBoardingView()
-
   }
 }

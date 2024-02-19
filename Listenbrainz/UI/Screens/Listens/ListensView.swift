e9d8e3a863f9f2688d85580e926fe571e6cf2342
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2024-02-19 18:28:01 +0530

    Update ListensView.swift

--- Listenbrainz/UI/Screens/Listens/ListensView.swift
+++ Listenbrainz/UI/Screens/Listens/ListensView.swift
@@ -14,32 +14,32 @@ struct ListensView: View {
   @State private var spotifyID: String = "https://open.spotify.com/track/0DHh3p0g7qmNNfNdRNwL6N"
   @State private var maxTimeStamp:String = "john"
   @State private var minTimeStamp:String = "123456"
-  
+
   var params: [String: String] {
     return ["max_ts": maxTimeStamp, "min_ts":minTimeStamp]
   }
-  
+
   var body: some View {
     VStack {
-      NavigationView {
-        SongDetailView()
-          .environmentObject(homeViewModel)
-        
-        //           TextField("Enter Spotify ID", text: $spotifyID)
-        //               .textFieldStyle(RoundedBorderTextFieldStyle())
-        //               .padding()
-        //
-        //           Button(action: {
-        //               spotifyManager.playSong(spotifyID: spotifyID)
-        //           }) {
-        //               Text("Play Song")
-        //                   .font(.headline)
-        //                   .padding()
-        //                   .background(Color.blue)
-        //                   .foregroundColor(.white)
-        //                   .cornerRadius(10)
-        //           }
-      }
+
+      SongDetailView()
+        .environmentObject(homeViewModel)
+
+      //           TextField("Enter Spotify ID", text: $spotifyID)
+      //               .textFieldStyle(RoundedBorderTextFieldStyle())
+      //               .padding()
+      //
+      //           Button(action: {
+      //               spotifyManager.playSong(spotifyID: spotifyID)
+      //           }) {
+      //               Text("Play Song")
+      //                   .font(.headline)
+      //                   .padding()
+      //                   .background(Color.blue)
+      //                   .foregroundColor(.white)
+      //                   .cornerRadius(10)
+      //           }
     }
   }
 }
+

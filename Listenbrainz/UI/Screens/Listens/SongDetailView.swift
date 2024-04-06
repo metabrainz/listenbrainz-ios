14a5f7c410f5828594ad89b701ad6e749fa11a9f
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2024-04-06 23:07:06 +0530

    Listens fix

--- Listenbrainz/UI/Screens/Listens/SongDetailView.swift
+++ Listenbrainz/UI/Screens/Listens/SongDetailView.swift
@@ -1,93 +1,99 @@
 import SwiftUI
 
 struct SongDetailView: View {
-    @EnvironmentObject var homeViewModel: HomeViewModel
-    @StateObject private var imageLoader = ImageLoader.shared
-    @State private var isLoading = true
-
-    var body: some View {
-        VStack {
-            List {
-                ForEach(homeViewModel.listens, id: \.recordingMsid) { listen in
-                    HStack {
-                      if let coverArtURL = listen.trackMetadata?.coverArtURL {
-                            AsyncImage(
-                                url: coverArtURL,
-                                scale: 0.1,
-                                transaction: Transaction(animation: nil),
-                                content: { phase in
-                                    switch phase {
-                                    case .success(let image):
-                                        image
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width: 50, height: 50)
-                                            .onAppear {
-                                                imageLoader.loadImage(url: coverArtURL) { loadedImage in
-                                                    if let uiImage = loadedImage {
-                                                        ImageCache.shared.insertImage(uiImage, for: coverArtURL)
-                                                    }
-                                                }
-                                            }
-
-                                    default:
-                                        if let cachedImage = ImageCache.shared.image(for: coverArtURL) {
-                                            Image(uiImage: cachedImage)
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 50, height: 50)
-                                        } else {
-                                            Image(systemName: "music.note")
-                                                .resizable()
-                                                .renderingMode(.template)
-                                                .foregroundColor(.orange)
-                                                .scaledToFit()
-                                                .frame(width: 50, height: 50)
-                                        }
-                                    }
-                                }
-                            )
-                            .frame(width: 50, height: 50)
-                        } else {
-                            Image(systemName: "music.note")
-                                .resizable()
-                                .renderingMode(.template)
-                                .foregroundColor(.orange)
-                                .scaledToFit()
-                                .frame(width: 50, height: 50)
-                        }
-
-                        VStack(alignment: .leading) {
-                          Text(listen.trackMetadata!.trackName)
-                                .lineLimit(1)
-                                .font(.headline)
-                          Text(listen.trackMetadata!.artistName)
-                                .lineLimit(1)
-                        }
-                    }
-                }
-                .navigationTitle("Listens")
-            }
-            .listStyle(PlainListStyle())
-            .listRowSeparator(.hidden)
-            .listRowInsets(EdgeInsets(top: 0, leading: 36, bottom: 0, trailing: 16))
-        }
-        .onAppear {
-            Task {
-                await withTaskGroup(of: Void.self) { group in
-                    for listen in homeViewModel.listens {
-                      if let coverArtURL = listen.trackMetadata?.coverArtURL {
-                            group.addTask {
-                                await imageLoader.loadImage(url: coverArtURL) { _ in }
+  @EnvironmentObject var homeViewModel: HomeViewModel
+  @StateObject private var imageLoader = ImageLoader.shared
+  @State private var isLoading = true
+  @Environment(\.colorScheme) var colorScheme
+  
+  var body: some View {
+    
+    ZStack {
+      colorScheme == .dark ? Color.backgroundColor : Color.white
+      
+      VStack {
+        ScrollView{
+          ForEach(homeViewModel.listens, id: \.recordingMsid) { listen in
+            HStack {
+              if let coverArtURL = listen.trackMetadata?.coverArtURL {
+                AsyncImage(
+                  url: coverArtURL,
+                  scale: 0.1,
+                  transaction: Transaction(animation: nil),
+                  content: { phase in
+                    switch phase {
+                    case .success(let image):
+                      image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                        .onAppear {
+                          imageLoader.loadImage(url: coverArtURL) { loadedImage in
+                            if let uiImage = loadedImage {
+                              ImageCache.shared.insertImage(uiImage, for: coverArtURL)
                             }
+                          }
                         }
+                      
+                    default:
+                      if let cachedImage = ImageCache.shared.image(for: coverArtURL) {
+                        Image(uiImage: cachedImage)
+                          .resizable()
+                          .scaledToFit()
+                          .frame(width: 50, height: 50)
+                      } else {
+                        Image(systemName: "music.note")
+                          .resizable()
+                          .renderingMode(.template)
+                          .foregroundColor(.orange)
+                          .scaledToFit()
+                          .frame(width: 50, height: 50)
+                      }
                     }
-                    await group.waitForAll()
-                }
-                isLoading = false
+                  }
+                )
+                .frame(width: 50, height: 50)
+              } else {
+                Image(systemName: "music.note")
+                  .resizable()
+                  .renderingMode(.template)
+                  .foregroundColor(.orange)
+                  .scaledToFit()
+                  .frame(width: 50, height: 50)
+              }
+              
+              VStack(alignment: .leading) {
+                Text(listen.trackMetadata!.trackName)
+                  .lineLimit(1)
+                  .font(.headline)
+                Text(listen.trackMetadata!.artistName)
+                  .lineLimit(1)
+              }
             }
+            .frame(maxWidth: .infinity, alignment: .leading) 
+          }
+          .navigationTitle("Listens")
         }
+        .listStyle(PlainListStyle())
+        .listRowSeparator(.hidden)
+        .listRowInsets(EdgeInsets(top: 0, leading: 36, bottom: 0, trailing: 16))
+      }
+      .onAppear {
+        Task {
+          await withTaskGroup(of: Void.self) { group in
+            for listen in homeViewModel.listens {
+              if let coverArtURL = listen.trackMetadata?.coverArtURL {
+                group.addTask {
+                  await imageLoader.loadImage(url: coverArtURL) { _ in }
+                }
+              }
+            }
+            await group.waitForAll()
+          }
+          isLoading = false
+        }
+      }
     }
+  }
+  
 }
-
-

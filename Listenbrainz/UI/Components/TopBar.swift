8b328f6223f905dd573afb0fd71ff5ee33d4de70
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2024-06-03 19:43:15 +0530

    Update TopBar.swift

--- Listenbrainz/UI/Components/TopBar.swift
+++ Listenbrainz/UI/Components/TopBar.swift
@@ -8,56 +8,61 @@
 import SwiftUI
 
 struct TopBar: View {
-  @Binding var isSettingsPressed: Bool
-  var customText: String
-  @Environment(\.colorScheme) var colorScheme
+    @Binding var isSettingsPressed: Bool
+    @Binding var isSearchActive: Bool
+    var customText: String
+    @Environment(\.colorScheme) var colorScheme
 
-  var body: some View {
-    HStack {
+    var body: some View {
+        HStack {
+            HStack(spacing: -13) {
+                Image("secondaryIcon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24, height: 24)
+                Image("primaryIcon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24, height: 24)
+            }
 
-      HStack(spacing:-13){
-        Image("secondaryIcon")
-          .resizable()
-          .scaledToFit()
-          .frame(width: 24, height: 24)
-        Image("primaryIcon")
-          .resizable()
-          .scaledToFit()
-          .frame(width: 24, height: 24)
-      }
+            Text(customText)
+                .foregroundColor(colorScheme == .dark ? .white : .black)
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.trailing, 20)
 
+            Spacer()
 
-      Text(customText)
-        .foregroundColor(colorScheme == .dark ? .white : .black)
-        .font(.title2)
-        .fontWeight(.bold)
-        .padding(.trailing,20)
+            Button(action: {
+                isSearchActive = true
+            }) {
+                Image(systemName: "magnifyingglass")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .padding(.trailing)
+                    .foregroundColor(Color.primary)
+            }
 
-      Spacer()
-
-      Button(action: {
-        isSettingsPressed.toggle()
-      }) {
-        Image(systemName: "gear")
-          .resizable()
-          .frame(width: 24, height: 24)
-          .padding(.trailing)
-          .foregroundColor(Color.primary)
-
-      }
+            Button(action: {
+                isSettingsPressed.toggle()
+            }) {
+                Image(systemName: "gear")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .padding(.trailing)
+                    .foregroundColor(Color.primary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.horizontal, 16)
+        .padding(.top, 70)
+        .padding(.bottom, 20)
     }
-    .frame(maxWidth: .infinity)
-    .padding(.horizontal, 16)
-    .padding(.top, 70)
-    .padding(.bottom, 20)
-  }
 }
 
-
-
 struct TopBar_Previews: PreviewProvider {
-  static var previews: some View {
-    TopBar(isSettingsPressed: .constant(false), customText: "feed")
-
-  }
+    static var previews: some View {
+        TopBar(isSettingsPressed: .constant(false), isSearchActive: .constant(false), customText: "Feed")
+    }
 }

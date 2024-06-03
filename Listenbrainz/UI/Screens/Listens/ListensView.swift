fcbcbfe140d8f8352d8d8c7188977cb17a190b93
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2024-06-03 19:43:19 +0530

    Update ListensView.swift

--- Listenbrainz/UI/Screens/Listens/ListensView.swift
+++ Listenbrainz/UI/Screens/Listens/ListensView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct ListensView: View {
 
   @EnvironmentObject var homeViewModel: HomeViewModel
-  @EnvironmentObject var spotifyManager: SpotifyManager
+  //  @EnvironmentObject var spotifyManager: SpotifyManager
   @State private var spotifyID: String = "https://open.spotify.com/track/0DHh3p0g7qmNNfNdRNwL6N"
   @State private var maxTimeStamp:String = "john"
   @State private var minTimeStamp:String = "123456"
   @State private var isSettingsPressed = false
+  @State private var isSearchActive = false
   @Environment(\.colorScheme) var colorScheme
 
   var params: [String: String] {
@@ -27,36 +28,39 @@ struct ListensView: View {
       colorScheme == .dark ? Color.backgroundColor : Color.white
 
       VStack {
+        if isSearchActive{
+          SearchUsersView(isSearchActive: $isSearchActive)
+        }
+        else{
+          TopBar(isSettingsPressed:$isSettingsPressed, isSearchActive: $isSearchActive, customText: "Listens")
 
-        TopBar(isSettingsPressed:$isSettingsPressed, customText: "Listens")
-
-        SongDetailView()
-          .frame(maxWidth: .infinity, alignment: .leading)
-          .background(colorScheme == .dark ? Color.black : Color.white)
-          .cornerRadius(10)
-          .shadow(radius: 2)
-          .sheet(isPresented: $isSettingsPressed) {
-            SettingsView()
-          }
+          SongDetailView()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(colorScheme == .dark ? Color.black : Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 2)
+            .sheet(isPresented: $isSettingsPressed) {
+              SettingsView()
+            }
 
 
-        //           TextField("Enter Spotify ID", text: $spotifyID)
-        //               .textFieldStyle(RoundedBorderTextFieldStyle())
-        //               .padding()
-        //
-        //           Button(action: {
-        //               spotifyManager.playSong(spotifyID: spotifyID)
-        //           }) {
-        //               Text("Play Song")
-        //                   .font(.headline)
-        //                   .padding()
-        //                   .background(Color.blue)
-        //                   .foregroundColor(.white)
-        //                   .cornerRadius(10)
-        //           }
+          //           TextField("Enter Spotify ID", text: $spotifyID)
+          //               .textFieldStyle(RoundedBorderTextFieldStyle())
+          //               .padding()
+          //
+          //           Button(action: {
+          //               spotifyManager.playSong(spotifyID: spotifyID)
+          //           }) {
+          //               Text("Play Song")
+          //                   .font(.headline)
+          //                   .padding()
+          //                   .background(Color.blue)
+          //                   .foregroundColor(.white)
+          //                   .cornerRadius(10)
+          //           }
+        }
+
       }
-
     }
   }
 }
-

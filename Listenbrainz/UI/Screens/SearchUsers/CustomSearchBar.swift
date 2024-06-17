956dbe69f3ee6cfd35ee82209c615db187c392af
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2024-06-17 20:46:12 +0530

    Add Custom Search bar

---
+++ Listenbrainz/UI/Screens/SearchUsers/CustomSearchBar.swift
@@ -0,0 +1,47 @@
+//
+//  CustomSearchBar.swift
+//  Listenbrainz
+//
+//  Created by Gaurav Bhardwaj on 17/06/24.
+//
+
+import SwiftUI
+
+
+struct CustomSearchBar: View {
+    @Binding var text: String
+    var onSearchButtonClicked: () -> Void
+
+    var body: some View {
+        HStack {
+            TextField("Search users", text: $text, onCommit: {
+                onSearchButtonClicked()
+            })
+            .padding(7)
+            .padding(.horizontal, 25)
+            .background(Color(.systemGray6))
+            .cornerRadius(8)
+            .overlay(
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.gray)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 8)
+
+                    if !text.isEmpty {
+                        Button(action: {
+                            self.text = ""
+                        }) {
+                            Image(systemName: "multiply.circle.fill")
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 8)
+                        }
+                    }
+                }
+            )
+            .padding(.horizontal, 10)
+        }
+        .padding(.top, 8)
+    }
+}
+

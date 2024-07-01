a775788d9d5b5efa8fc23bfb503b3bf5755b9e1d
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2024-07-01 20:47:45 +0530

    Update model to hold value changes

--- Listenbrainz/Model/Feed/Event.swift
+++ Listenbrainz/Model/Feed/Event.swift
@@ -9,22 +9,37 @@ import Foundation
 
 
 // MARK: - Event
-struct Event: Codable, Identifiable {
-  let created: Int
-  let eventType: String
-  let hidden: Bool
-  let metadata: Metadata
-  let userName: String
-  var id: Int {
-    created
-  }
+struct Event: Codable, Identifiable, TrackMetadataProvider {
+    let created: Int
+    let eventType: String
+    let hidden: Bool
+    let metadata: Metadata
+    let id: Int?
+    let userName: String
 
-  enum CodingKeys: String, CodingKey {
-      case created
-      case eventType = "event_type"
-      case hidden, metadata
-      case userName = "user_name"
-  }
+    enum CodingKeys: String, CodingKey {
+        case created
+        case eventType = "event_type"
+        case hidden, metadata, id
+        case userName = "user_name"
+    }
+
+    var trackName: String? { metadata.trackMetadata?.trackName }
+    var artistName: String? { metadata.trackMetadata?.artistName }
+    var coverArtURL: URL? { metadata.trackMetadata?.coverArtURL }
+    var originURL: String? { metadata.trackMetadata?.additionalInfo?.originURL }
+    var recordingMbid: String? { metadata.trackMetadata?.additionalInfo?.recordingMbid }
+    var recordingMsid: String? { metadata.trackMetadata?.additionalInfo?.recordingMsid }
 }
+
+protocol TrackMetadataProvider {
+    var trackName: String? { get }
+    var artistName: String? { get }
+    var coverArtURL: URL? { get }
+    var originURL: String? { get }
+    var recordingMbid: String? { get }
+    var recordingMsid: String? { get }
+}
+
 
 
--- Listenbrainz/Model/Feed/FeedAdditionalInfo.swift
+++ Listenbrainz/Model/Feed/FeedAdditionalInfo.swift
@@ -10,9 +10,11 @@ import Foundation
 struct FeedAdditionalInfo: Codable {
     let originURL: String?
     let recordingMbid: String?
+    let recordingMsid: String
 
     enum CodingKeys: String, CodingKey {
         case originURL = "origin_url"
         case recordingMbid = "recording_mbid"
+        case recordingMsid = "recording_msid"
     }
 }

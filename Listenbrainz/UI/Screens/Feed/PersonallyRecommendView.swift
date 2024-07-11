4c8b60669473c0cd7b345037169c886132d0a236
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2024-07-11 17:15:05 +0530

    Update PersonallyRecommendView.swift

--- Listenbrainz/UI/Screens/Feed/PersonallyRecommendView.swift
+++ Listenbrainz/UI/Screens/Feed/PersonallyRecommendView.swift
@@ -8,37 +8,64 @@
 import SwiftUI
 
 struct RecommendToUsersPersonallyView: View {
-    @State private var blurbContent: String = ""
-    @State private var users: String = ""
-    var item: TrackMetadataProvider
-    var userName: String
-    var userToken: String
-    @EnvironmentObject var viewModel: FeedViewModel
-   var dismissAction: () -> Void
+  @State private var blurbContent: String = ""
+  @State private var users: String = ""
+  var item: TrackMetadataProvider
+  var userName: String
+  var userToken: String
+  @EnvironmentObject var viewModel: FeedViewModel
+  var dismissAction: () -> Void
 
-    var body: some View {
-        VStack {
-            TextField("Blurb Content", text: $blurbContent)
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+  var body: some View {
+    VStack(alignment:.leading, spacing:10) {
 
-            TextField("Users (comma separated)", text: $users)
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+      Text("Recommend \(item.trackName ?? "")")
 
-            Button(action: {
-                let usersArray = users.split(separator: ",").map { String($0).trimmingCharacters(in: .whitespacesAndNewlines) }
-                viewModel.recommendToUsersPersonally(userName: userName, item: item, users: usersArray, blurbContent: blurbContent, userToken: userToken)
-              dismissAction()
-            }) {
-                Text("Recommend Personally")
-                    .padding()
-                    .background(Color.green)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
-            }
-        }
+
+
+      TextField("Add Followers", text: $users)
         .padding()
-    }
-}
+        .textFieldStyle(RoundedBorderTextFieldStyle())
 
+      Text("Leave a message (optional)")
+
+
+      TextField("You will love this song because...", text: $blurbContent)
+        .padding()
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+
+      HStack {
+          Spacer()
+          Text("\(blurbContent.count) / 280")
+              .font(.caption)
+              .foregroundColor(.gray)
+      }
+
+
+
+
+      HStack {
+        Spacer()
+        Button(action: {
+          dismissAction()
+        }) {
+          Text("Cancel")
+            .padding()
+        }
+
+        Button(action: {
+          let usersArray = users.split(separator: ",").map { String($0).trimmingCharacters(in: .whitespacesAndNewlines) }
+          viewModel.recommendToUsersPersonally(userName: userName, item: item, users: usersArray, blurbContent: blurbContent, userToken: userToken)
+          dismissAction()
+        }) {
+          Text("Send recommendation")
+            .padding()
+            .background(Color.yimGreen)
+            .foregroundColor(.white)
+        }
+
+      }
+      .padding()
+    }
+  }
+}

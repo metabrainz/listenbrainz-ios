f2f09509d19f9ac781ca8608067795d79f776614
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2024-07-11 17:14:50 +0530

    Update UI of write a review view

--- Listenbrainz/UI/Screens/Feed/WriteAReviewView.swift
+++ Listenbrainz/UI/Screens/Feed/WriteAReviewView.swift
@@ -11,51 +11,101 @@ struct WriteAReviewView<T: TrackMetadataProvider>: View {
     @EnvironmentObject var viewModel: FeedViewModel
     @State private var text: String = ""
     @State private var rating: Int = 0
-    @State private var language: String = "en"
+    @State private var language: String = "English"
+    @State private var agreedToTerms: Bool = false
     @Binding var isPresented: Bool
+    @Environment (\.colorScheme) var colorScheme
     var item: T
     var userToken: String
     var userName: String
     var dismissAction: () -> Void
 
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text("Write a Review")
-                .font(.headline)
+  var body: some View {
 
-            TextField("Your Review", text: $text)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.vertical)
+      VStack(alignment: .leading, spacing: 10) {
+        HStack {
+          Spacer()
+          Image("critiquebrainz-logo")
+            .resizable()
+            .frame(width: 200, height: 30)
+            .padding(.top, 5)
+          Spacer()
+        }
+        .padding(.trailing, 10)
 
-            Picker("Rating (optional):", selection: $rating) {
-                ForEach(1..<6) { index in
-                    Text("\(index) Star\(index > 1 ? "s" : "")").tag(index)
-                }
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            .padding(.vertical)
+        Divider()
 
-            Picker("Language of your review:", selection: $language) {
-                ForEach(["English", "Spanish", "French", "German", "Chinese"], id: \.self) { lang in
-                    Text(lang).tag(lang)
-                }
-            }
-            .pickerStyle(MenuPickerStyle())
-            .padding(.vertical)
+        HStack(spacing: 0) {
+          Text("You are reviewing ")
 
-            Button(action: {
-                writeReview()
-                dismissAction()
-            }) {
-                Text("Submit Review")
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
+          Text("\(item.trackName ?? "this track") ")
+            .fontWeight(.bold)
+
+          Text("for ")
+
+          Text("CritiqueBrainz.")
+            .foregroundColor(Color.LbPurple)
+            .fontWeight(.bold)
+            .onTapGesture {
+              if let url = URL(string: "https://critiquebrainz.org") {
+                UIApplication.shared.open(url)
+              }
             }
         }
-        .padding()
+        .font(.subheadline)
+
+        DismissableTextView(text: $text)
+          .frame(height: 150)
+          .padding(.vertical, 5)
+
+        HStack {
+          Text("Rating (optional):")
+            .padding(.top, 5)
+
+          StarRatingView(rating: $rating)
+            .padding(.vertical, 5)
+        }
+
+        HStack {
+          Text("Language of your review:")
+            .padding(.top, 5)
+
+          Picker("Select Language", selection: $language) {
+            ForEach(["English", "Spanish", "French", "German", "Chinese"], id: \.self) { lang in
+              Text(lang).tag(lang)
+            }
+          }
+          .pickerStyle(MenuPickerStyle())
+          .padding(.vertical, 5)
+        }
+
+        Toggle(isOn: $agreedToTerms) {
+          Text("You acknowledge and agree that your contributed reviews to CritiqueBrainz are licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) license. You agree to license your work under this license. You represent and warrant that you own or control all rights in and to the work, that nothing in the work infringes the rights of any third-party, and that you have the permission to use and to license the work under the selected Creative Commons license. Finally, you give the MetaBrainz Foundation permission to license this content for commercial use outside of Creative Commons licenses in order to support the operations of the organization.")
+            .font(.system(size: 10))
+        }
+        .toggleStyle(CheckboxToggleStyle())
+        .padding(.vertical, 5)
+
+        HStack{
+          Spacer()
+          Button(action: {
+            if agreedToTerms {
+              writeReview()
+              dismissAction()
+            } else {
+              print("User must agree to the terms")
+            }
+          }) {
+            Text("Submit Review To CritiqueBrainz")
+              .background(agreedToTerms ? Color.green : Color.yimGreen)
+              .foregroundColor(.white)
+          }
+          .disabled(!agreedToTerms)
+        }
+      }
+      .padding(.trailing)
     }
+
 
     private func writeReview() {
         guard let recordingMsid = item.recordingMsid else {
@@ -75,4 +125,37 @@ struct WriteAReviewView<T: TrackMetadataProvider>: View {
         )
     }
 }
+
+
+struct StarRatingView: View {
+    @Binding var rating: Int
+
+    var body: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: index <= rating ? "star.fill" : "star")
+                    .foregroundColor(index <= rating ? .yellow : .gray)
+                    .onTapGesture {
+                        rating = index
+                    }
+            }
+        }
+    }
+}
+
+struct CheckboxToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            Image(systemName: configuration.isOn ? "checkmark.square.fill" : "square")
+                .resizable()
+                .frame(width: 20, height: 20)
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+            configuration.label
+        }
+    }
+}
+
+
 

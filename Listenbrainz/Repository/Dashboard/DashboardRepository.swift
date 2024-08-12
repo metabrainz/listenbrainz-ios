1a2fccf133781fbd16342082de394dfa9812cc45
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2024-08-12 21:39:24 +0530

    Update repository

--- Listenbrainz/Repository/Dashboard/DashboardRepository.swift
+++ Listenbrainz/Repository/Dashboard/DashboardRepository.swift
@@ -13,4 +13,11 @@ protocol DashboardRepository{
   func getListenCount(userName:String) -> AnyPublisher<CountPayload,AFError>
   func getFollowers(userName:String) -> AnyPublisher<Followers, AFError>
   func getFollowing(userName:String) -> AnyPublisher<Following, AFError>
+
+  // MARK: - Statistics
+  func getListeningActivity(userName: String) -> AnyPublisher<ActivityPayload, AFError>
+  func getTopArtists(userName:String) -> AnyPublisher<ArtistsPayload, AFError>
+  func getTopAlbums(userName:String) -> AnyPublisher<AlbumsPayload, AFError>
+  func getTopTracks(userName:String) -> AnyPublisher<TracksPayload, AFError>
+  func getDailyActivity(userName:String) -> AnyPublisher<DailyActivityPayload, AFError>
 }
--- Listenbrainz/Repository/Dashboard/DashboardRepositoryImpl.swift
+++ Listenbrainz/Repository/Dashboard/DashboardRepositoryImpl.swift
@@ -46,8 +46,95 @@ class DashboardRepositoryImpl: DashboardRepository {
       .value()
       .eraseToAnyPublisher()
   }
-  
-}
+
+  func getListeningActivity(userName: String) -> AnyPublisher<ActivityPayload, AFError> {
+         let urlString = "\(BuildConfiguration.shared.API_LISTENBRAINZ_BASE_URL)/stats/user/\(userName)/listening-activity"
+         print("Fetching from URL: \(urlString)")
+
+         guard let url = URL(string: urlString) else {
+             return Fail(error: AFError.invalidURL(url: urlString))
+                 .eraseToAnyPublisher()
+         }
+
+         return AF.request(url, method: .get)
+             .validate()
+             .publishDecodable(type: ActivityResponse.self) 
+             .value()
+             .map { $0.payload }
+             .eraseToAnyPublisher()
+     }
+
+  func getTopArtists(userName: String) -> AnyPublisher<ArtistsPayload, AFError> {
+    let urlString = "\(BuildConfiguration.shared.API_LISTENBRAINZ_BASE_URL)/stats/user/\(userName)/artists"
+    print("Fetching from URL: \(urlString)")
+
+    guard let url = URL(string: urlString) else {
+        return Fail(error: AFError.invalidURL(url: urlString))
+            .eraseToAnyPublisher()
+    }
+
+    return AF.request(url, method: .get)
+        .validate()
+        .publishDecodable(type: TopArtists.self)
+        .value()
+        .map { $0.payload }
+        .eraseToAnyPublisher()
+     }
+
+  func getTopAlbums(userName: String) -> AnyPublisher<AlbumsPayload, AFError> {
+    let urlString = "\(BuildConfiguration.shared.API_LISTENBRAINZ_BASE_URL)/stats/user/\(userName)/releases"
+    print("Fetching from URL: \(urlString)")
+
+    guard let url = URL(string: urlString) else {
+        return Fail(error: AFError.invalidURL(url: urlString))
+            .eraseToAnyPublisher()
+    }
+
+    return AF.request(url, method: .get)
+        .validate()
+        .publishDecodable(type: TopAlbums.self)
+        .value()
+        .map { $0.payload }
+        .eraseToAnyPublisher()
+
+  }
+
+  func getTopTracks(userName: String) -> AnyPublisher<TracksPayload, AFError> {
+    let urlString = "\(BuildConfiguration.shared.API_LISTENBRAINZ_BASE_URL)/stats/user/\(userName)/recordings"
+    print("Fetching from URL: \(urlString)")
+
+    guard let url = URL(string: urlString) else {
+        return Fail(error: AFError.invalidURL(url: urlString))
+            .eraseToAnyPublisher()
+    }
+
+    return AF.request(url, method: .get)
+        .validate()
+        .publishDecodable(type: TopTracks.self)
+        .value()
+        .map { $0.payload }
+        .eraseToAnyPublisher()
+    }
+
+  func getDailyActivity(userName: String) -> AnyPublisher<DailyActivityPayload, AFError> {
+    let urlString = "\(BuildConfiguration.shared.API_LISTENBRAINZ_BASE_URL)/stats/user/\(userName)/daily-activity"
+    print("Fetching from URL: \(urlString)")
+
+    guard let url = URL(string: urlString) else {
+        return Fail(error: AFError.invalidURL(url: urlString))
+            .eraseToAnyPublisher()
+    }
+
+    return AF.request(url, method: .get)
+        .validate()
+        .publishDecodable(type: DailyActivityResponse.self)
+        .value()
+        .map { $0.payload }
+        .eraseToAnyPublisher()
+  }
+
+
+ }
 
 
 

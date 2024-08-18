e8cf990ea8a5780db7feee78b0f1f0778a23790c
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2024-08-19 05:17:56 +0530

    Create CreatedForView.swift

---
+++ Listenbrainz/UI/Screens/DashboardComponents/CreatedForView.swift
@@ -0,0 +1,53 @@
+//
+//  CreatedForView.swift
+//  Listenbrainz
+//
+//  Created by Gaurav Bhardwaj on 19/08/24.
+//
+
+
+import SwiftUI
+
+struct CreatedForYouView: View {
+  @EnvironmentObject var viewModel: DashboardViewModel
+
+  var body: some View {
+    ScrollView{
+      VStack(alignment: .leading) {
+        ForEach(viewModel.createdForYou.indices, id: \.self) { index in
+          let playlist = viewModel.createdForYou[index]
+          let imageName = "green-\(index + 1)"
+
+          ZStack(alignment:.leading) {
+            Image(imageName)
+              .resizable()
+              .aspectRatio(contentMode: .fill)
+              .frame(height: 200)
+              .clipped()
+
+            Text(playlist.title.components(separatedBy: ",").first ?? "")
+              .font(.system(size: 30, weight: .bold))
+              .padding()
+              .foregroundColor(Color.LbPurple)
+          }
+          .onTapGesture {
+            openPlaylistLink(playlist.identifier)
+          }
+          .cornerRadius(10)
+          .shadow(radius: 5)
+          .padding(.vertical, 8)
+        }
+      }
+      .onAppear {
+        viewModel.getCreatedForYou(username: viewModel.userName)
+      }
+    }
+  }
+
+    func openPlaylistLink(_ urlString: String) {
+      if let url = URL(string: urlString) {
+        UIApplication.shared.open(url)
+      }
+    }
+  }
+

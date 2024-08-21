4a6e7ed05b22b2268671c05bb6078e7e6a4e9425
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2024-08-21 19:28:24 +0530

    Update NavigationViewModel.swift

--- Listenbrainz/ViewModel/NavigationViewModel.swift
+++ Listenbrainz/ViewModel/NavigationViewModel.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 class UserSelection: ObservableObject {
     @Published var selectedUserName: String = ""
-    @AppStorage(Strings.AppStorageKeys.userName) private var userName: String = ""
 
     func selectUserName(_ username: String) {
-        if username.isEmpty {
-            selectedUserName = userName
-        } else if selectedUserName != username {
-            selectedUserName = username
-        }
+        selectedUserName = username
+    }
+
+    func resetUserName() {
+        selectedUserName = ""
     }
 }
+
 
 
 

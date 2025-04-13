06312c1d57e8a588e84a40a875e0f0eea31e9cfa
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2025-04-13 23:12:19 +0530

    Format file

--- Listenbrainz/UI/Components/SettingsView.swift
+++ Listenbrainz/UI/Components/SettingsView.swift
@@ -9,47 +9,47 @@ import SwiftUI
 
 
 struct SettingsView: View {
-  @AppStorage(Strings.AppStorageKeys.isDarkMode) private var isDarkMode = true
-  @AppStorage(Strings.AppStorageKeys.userToken) private var userToken: String = ""
-  @AppStorage(Strings.AppStorageKeys.userName) private var userName: String = ""
-  @EnvironmentObject var homeViewModel: HomeViewModel
-  @EnvironmentObject var feedViewModel: FeedViewModel
-  @Environment(\.dismiss) var dismiss
-
-
-  var body: some View  {
-    NavigationView {
-      Form{
-        Section(header: Text("Turn On Dark Mode"),
-                content: {
-          HStack{
-            Toggle("Dark Mode", isOn: $isDarkMode)
-          }
-        })
-        Section(header: Text("Enter User Token"),
-                footer: Text("Enter User Token from https://listenbrainz.org/profile/"),
-                content: {
-          TextField("Enter User Token", text: $userToken)
-        })
-        Section(header: Text("Enter User Name"),
-                content: {
-          TextField("Enter User Name", text: $userName)
-        })
-      }
-      .navigationBarItems(
-        trailing: Button(action: {
-          homeViewModel.requestMusicData(userName: userName)
-          dismiss()
-        }, label: {
-          Text("Save")
-        }))
-      .navigationBarTitle(Text("Settings"))
+    @AppStorage(Strings.AppStorageKeys.isDarkMode) private var isDarkMode = true
+    @AppStorage(Strings.AppStorageKeys.userToken) private var userToken: String = ""
+    @AppStorage(Strings.AppStorageKeys.userName) private var userName: String = ""
+    @EnvironmentObject var homeViewModel: HomeViewModel
+    @EnvironmentObject var feedViewModel: FeedViewModel
+    @Environment(\.dismiss) var dismiss
+    
+    
+    var body: some View  {
+        NavigationView {
+            Form{
+                Section(header: Text("Turn On Dark Mode"),
+                        content: {
+                    HStack{
+                        Toggle("Dark Mode", isOn: $isDarkMode)
+                    }
+                })
+                Section(header: Text("Enter User Token"),
+                        footer: Text("Enter User Token from https://listenbrainz.org/profile/"),
+                        content: {
+                    TextField("Enter User Token", text: $userToken)
+                })
+                Section(header: Text("Enter User Name"),
+                        content: {
+                    TextField("Enter User Name", text: $userName)
+                })
+            }
+            .navigationBarItems(
+                trailing: Button(action: {
+                    homeViewModel.requestMusicData(userName: userName)
+                    dismiss()
+                }, label: {
+                    Text("Save")
+                }))
+            .navigationBarTitle(Text("Settings"))
+        }
+        .preferredColorScheme(isDarkMode ? .dark : .light)
     }
-    .preferredColorScheme(isDarkMode ? .dark : .light)
-  }
-
+    
 }
 
 #Preview{
     SettingsView()
-  }
+}

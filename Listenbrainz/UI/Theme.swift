824131877ff3ad11312396012dc039416d2ec00c
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2025-04-13 23:50:27 +0530

    Add more colors

--- Listenbrainz/UI/Theme.swift
+++ Listenbrainz/UI/Theme.swift
@@ -41,12 +41,42 @@ struct Spacings {
 
 protocol AppColorScheme {
     var background: Color { get }
+    var onBackground: Color { get }
+    var level1: Color { get }
+    var level2: Color { get }
+    var lbSignature: Color { get }
+    var lbSignatureSecondary: Color { get }
+    var lbSignatureInverse: Color { get }
+    var onLbSignature: Color { get }
+    var text: Color { get }
+    var listenText: Color { get }
+    var hint: Color { get }
 }
 
 struct DarkColors: AppColorScheme {
     let background: Color = Color(hex: "FF292929")
+    let onBackground: Color = Color.white
+    let level1: Color = Color.black
+    let level2: Color = Color(hex: "FF3B3B3B")
+    let lbSignature: Color = Color.lb_purple_night
+    let lbSignatureSecondary: Color = Color.lb_yellow
+    let lbSignatureInverse: Color = Color.lb_orange
+    let onLbSignature: Color = Color.black
+    let text: Color = Color.white
+    let listenText: Color = Color.white
+    let hint: Color = Color(hex: "FF8C8C8C")
 }
 
 struct LightColors: AppColorScheme {
     let background: Color = Color.white
+    let onBackground: Color = Color.black
+    let level1: Color = Color.white
+    let level2: Color = Color(hex: "FFE3E3E3")
+    let lbSignature: Color = Color.lb_purple
+    let lbSignatureSecondary: Color = Color.lb_yellow
+    let lbSignatureInverse: Color = Color.lb_orange_day
+    let onLbSignature: Color = Color.white
+    let text: Color = Color.black
+    let listenText: Color = Color.lb_purple
+    let hint: Color = Color(hex: "FF707070")
 }

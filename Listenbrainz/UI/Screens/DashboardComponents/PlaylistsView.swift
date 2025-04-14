8e696ec4e775f30b31d92567fb94408fff4daacb
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2025-04-14 14:06:22 +0530

    Update PlaylistsView.swift

--- Listenbrainz/UI/Screens/DashboardComponents/PlaylistsView.swift
+++ Listenbrainz/UI/Screens/DashboardComponents/PlaylistsView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct PlaylistView: View {
     @EnvironmentObject var viewModel: DashboardViewModel
-    @Environment(\.colorScheme) var colorScheme
-  @AppStorage(Strings.AppStorageKeys.userName) private var userName: String = ""
+    @EnvironmentObject var theme: Theme
+    @AppStorage(Strings.AppStorageKeys.userName) private var userName: String = ""
 
     var body: some View {
         ScrollView {
-            VStack(spacing: 16) {
+            VStack(spacing: theme.spacings.vertical) {
                 if viewModel.playlists.isEmpty {
                     Text("No playlists available")
                         .foregroundColor(.gray)
@@ -28,51 +28,63 @@ struct PlaylistView: View {
                                     playlistName: playlist.title
                                 ).environmentObject(viewModel)
                             ) {
-                                VStack(alignment: .leading, spacing: 8) {
-                                    Text(playlist.title)
-                                        .font(.system(size: 30))
-                                        .foregroundColor(colorScheme == .dark ? .white : .black)
-                                    Text(playlist.annotation ?? "")
-                                        .font(.system(size: 20, weight: .bold))
-                                        .foregroundColor(Color.lightPink)
-                                    Text("Created: \(formattedDate(playlist.date))")
-                                        .font(.system(size: 20, weight: .bold))
-                                        .foregroundColor(Color.lightPink)
-                                    Text("Last modified: \(formattedDate(playlist.extensionData.playlistInfo.lastModifiedAt))")
-                                        .font(.system(size: 20, weight: .bold))
-                                        .foregroundColor(Color.lightPink)
-                                }
-                                .padding()
-                                .frame(width: UIScreen.main.bounds.size.width - 10, height: 200)
-                                .cornerRadius(12)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 12)
-                                        .stroke(Color.gray, lineWidth: 1)
+                                PlaylistCardView(
+                                    playlist: playlist,
+                                    playlistId: playlistId
                                 )
                             }
                         }
                     }
                 }
             }
-            .padding()
         }
         .onAppear {
             viewModel.getPlaylists(username: userName)
         }
     }
+}
 
-    private func formattedDate(_ dateString: String) -> String {
-        let formatter = ISO8601DateFormatter()
-        formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+struct PlaylistCardView: View {
+    @EnvironmentObject var theme: Theme
+    
+    let playlist: Playlist
+    let playlistId: String
 
-        if let date = formatter.date(from: dateString) {
-            let displayFormatter = DateFormatter()
-            displayFormatter.dateFormat = "dd/MM/yyyy"
-            return displayFormatter.string(from: date)
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(playlist.title)
+                .font(.system(size: 30))
+                .foregroundColor(theme.colorScheme.text)
+            Text(playlist.annotation ?? "")
+                .font(.system(size: 20, weight: .bold))
+                .foregroundColor(Color.lightPink)
+            Text("Created: \(formattedDate(playlist.date))")
+                .font(.system(size: 20, weight: .bold))
+                .foregroundColor(Color.lightPink)
+            Text("Last modified: \(formattedDate(playlist.extensionData.playlistInfo.lastModifiedAt))")
+                .font(.system(size: 20, weight: .bold))
+                .foregroundColor(Color.lightPink)
         }
-        return dateString
+        .frame(maxWidth: UIScreen.main.bounds.width)
+        .padding()
+        .cornerRadius(12)
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color.gray, lineWidth: 1)
+        )
+        .padding(.horizontal, theme.spacings.horizontal)
     }
 }
 
 
+private func formattedDate(_ dateString: String) -> String {
+    let formatter = ISO8601DateFormatter()
+    formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
 
+    if let date = formatter.date(from: dateString) {
+        let displayFormatter = DateFormatter()
+        displayFormatter.dateFormat = "dd/MM/yyyy"
+        return displayFormatter.string(from: date)
+    }
+    return dateString
+}

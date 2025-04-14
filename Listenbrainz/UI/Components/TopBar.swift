88a7901646283dc788cae726d0e1ebe6bf7dc5dd
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2025-04-14 14:57:52 +0530

    Update TopBar.swift

--- Listenbrainz/UI/Components/TopBar.swift
+++ Listenbrainz/UI/Components/TopBar.swift
@@ -14,59 +14,55 @@ struct TopBar: View {
     @EnvironmentObject var theme: Theme
 
     var body: some View {
-        VStack {
-            HStack {
-                HStack(spacing: -13) {
-                    Image("secondaryIcon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
-                    Image("primaryIcon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
-                }
-
-                Text(customText)
-                    .foregroundColor(theme.colorScheme.text)
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .padding(.trailing, 20)
-
-                Spacer()
-
-                Button(action: {
-                    isSearchActive.toggle()
-                }) {
-                    Image("search")
-                        .resizable()
-                        .renderingMode(.template)
-                        .frame(width: 24, height: 24)
-                        .padding(.trailing)
-                        .foregroundColor(theme.colorScheme.text.opacity(0.7))
-                }
-
-                Button(action: {
-                    isSettingsPressed.toggle()
-                }) {
-                    Image("settings")
-                        .resizable()
-                        .renderingMode(.template)
-                        .frame(width: 24, height: 24)
-                        .padding(.trailing)
-                        .foregroundColor(theme.colorScheme.text.opacity(0.7))
-                }
+        HStack {
+            HStack(spacing: -13) {
+                Image("secondaryIcon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24, height: 24)
+                Image("primaryIcon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24, height: 24)
             }
-            .fullScreenCover(isPresented: $isSearchActive) {
-              SearchUsersView(isSearchActive: $isSearchActive)
-                .edgesIgnoringSafeArea(.all)
+            
+            Text(customText)
+                .foregroundColor(theme.colorScheme.text)
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.trailing, 20)
+            
+            Spacer()
+            
+            Button(action: {
+                isSearchActive.toggle()
+            }) {
+                Image("search")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 24, height: 24)
+                    .padding(.trailing)
+                    .foregroundColor(theme.colorScheme.text.opacity(0.7))
             }
-            .frame(maxWidth: .infinity)
-            .padding(.horizontal, 16)
-            .padding(.top, 70)
-            .padding(.bottom, 20)
-
+            
+            Button(action: {
+                isSettingsPressed.toggle()
+            }) {
+                Image("settings")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 24, height: 24)
+                    .padding(.trailing, 16)
+                    .foregroundColor(theme.colorScheme.text.opacity(0.7))
+            }
         }
+        .fullScreenCover(isPresented: $isSearchActive) {
+            SearchUsersView(isSearchActive: $isSearchActive)
+                .edgesIgnoringSafeArea(.all)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.horizontal, theme.spacings.horizontal)
+        .padding(.top, 70)
     }
 }
 
